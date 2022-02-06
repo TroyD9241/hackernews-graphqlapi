74f0266eda59addc220256782861757e99dee26b
Troy <troyd9216@gmail.com>
2022-02-06 17:57:26 -0600

    updated

---
+++ client/src/components/LinkList.js
@@ -0,0 +1,30 @@
+import Link from "./Link";
+import { useQuery, gql } from "@apollo/client";
+const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        url
+        description
+      }
+    }
+  }
+`;
+
+const LinkList = () => {
+  const { data } = useQuery(FEED_QUERY);
+  return (
+    <div>
+      {data && (
+        <>
+          {data.feed?.links?.map((link) => {
+            return <Link key={link.id} link={link} />;
+          })}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default LinkList;
