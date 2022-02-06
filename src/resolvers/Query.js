5a16d48a3c42adf9bc8d0fc3d16f08e8b0167547
Troy <troyd9216@gmail.com>
2022-02-05 21:02:59 -0600

    added filtering, and finished initial api

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,21 @@
-async function feed(parents, args, context) {
-  const feedData = await context.prisma.link.findMany();
-  return feedData;
+async function feed(parent, args, context, info) {
+  const where = args.filter
+    ? {
+        OR: [
+          { description: { contains: args.filter } },
+          { url: { contains: args.filter } },
+        ],
+      }
+    : {};
+
+  const links = await context.prisma.link.findMany({
+    where,
+    skip: args.skip,
+    take: args.take,
+    orderBy: args.orderBy,
+  });
+  const count = await context.prisma.link.count({ where });
+  return { links, count };
 }
 
 async function findLink(parent, { id }, context) {
